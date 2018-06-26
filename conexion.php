d34f1ef5f93018932947882db01bf108a0b84f87
Steven Avila <sgavila1@misena.edu.co>
2018-06-26 16:50:44 -0500

    conexion
    Conexion a base de datos

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php
+$conexion = mysqli_connect("localhost","root","","bd_imdatyt");
+//if(!$conexion) {
+  //  echo 'Error al conectar a la base de datos';
+//}
+//else{
+    //echo 'conectado a la base de datos';
+//}
